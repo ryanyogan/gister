c24eaa3cfff396cca4a12be97a3536b211d4c8d0
Ryan Yogan <ryanyogan@gmail.com>
2023-12-28 12:57:39 -0600

    feat // rust backend services wired up

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use commands::{
+    create_new_gist, get_commits_to_repository, get_gist_content, get_gists_for_authenticated_user,
+    get_public_gists, get_public_repositories, get_repositories_for_authenticated_user,
+    get_users_associated_with_repository,
+};
 
+mod api;
+mod commands;
 mod error;
 mod models;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            create_new_gist,
+            get_public_gists,
+            get_repositories_for_authenticated_user,
+            get_gist_content,
+            get_users_associated_with_repository,
+            get_commits_to_repository,
+            get_public_repositories,
+            get_gists_for_authenticated_user
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
